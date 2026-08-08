84581e9fd1727684d5062f0adab3330d10c3aa88
chrispycodingz <chriscosmos44@gmail.com>
2026-08-09 09:50:11 +1000

    Extra credit CE feature implemented. Assignment complete. Merging to main.

--- script.js
+++ script.js
@@ -33,14 +33,6 @@ let sum = null;
 
 
 
-
-
-
-//NOTES: CAN ONLY FIT 11 figures on display. 12 IF NUMERIC VALUE IS 1.
-
-//TODO: Following use of equals operator, the b variable sticks and is appended upon with new numeric value rather than clearing.
-// must track whether 
-
 //Initialize all selector variables
 const display = document.querySelector('.display');
 const operators = ['.', '+', '-', 'X', '/', '%', 'AC', 'CE'];
@@ -48,6 +40,11 @@ const numeric = document.querySelectorAll('.buttons .numeric');
 const operator = document.querySelectorAll('.buttons .operator');
 const equal = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
+const ce = document.querySelector('.CE');
+
+//Non functional selectors
+const decimal = document.querySelector('.decimal');
+const percent = document.querySelector('.percent');
 
 
 
@@ -58,6 +55,7 @@ function update() {
     let operandButtonPressed = false;
     let aBypass = false;
     let sumDisplayed = false;
+    let forward = false;
 
     function nullify() {
         a = null;
@@ -70,13 +68,38 @@ function update() {
         sumDisplayed = false;
     }
 
+    //Non functional events
+    percent.addEventListener('click', () => {
+        display.textContent = `No %'s ;)`
+    })
+
+    decimal.addEventListener('click', () => {
+        display.textContent = 'No floats ;)';
+        nullify();
+    })
+
+    //Functional events
+    ce.addEventListener('click', () => {
+        if (sumDisplayed === false && operandButtonPressed === false && a !== null) {
+            a = null;
+            display.textContent = '0';
+        } else if (operandButtonPressed && b !== null && sumDisplayed === false) {
+            b = null;
+            display.textContent = '0';
+        } else if (sumDisplayed && b !== null) {
+            a = sum;
+            b = null;
+            display.textContent = '0';
+        } else if (sumDisplayed && b === null) {
+            display.textContent = '0';
+        }
+        
+    })
+
     //Clear button function. TOTALLY WORKING!
     clear.addEventListener('click', () => {
         nullify();
         display.textContent = 0;
-        console.log(a);
-        console.log(b);
-        console.log(operand)
     });
     
     //Equal button function.. TOTALLY WORKING! (INCULDING 0 DIVISION!)
@@ -85,22 +108,17 @@ function update() {
             sum = operate(+a, +b, operand);
             sum = Math.round(sum * 100) / 100;
             display.textContent = sum;
+            forward = true;
             sumDisplayed = true;
             equalButtonPressed = true;
         } else {
-            display.textContent = 'Bozo alert';
+            display.textContent = 'Bozo alert!';
             nullify();
         }
 
         if (b == 0 && operand === '/') {
             display.textContent = 'Zero Div Err';
-            console.log(a);
-            console.log(b);
-            console.log(operand);
             nullify();
-            console.log(a);
-            console.log(b);
-            console.log(operand);
         }
         
     });
@@ -115,16 +133,18 @@ function update() {
                     a = a + String(button.textContent);
                     display.textContent = a;
                 }
-            } else if (sumDisplayed && b !== null) {
+            } else if (sumDisplayed && b !== null && forward === true) {
+                
                 nullify();
                 a = String(button.textContent);
                 display.textContent = a;
             }
 
             if (operandButtonPressed && b === null || b == 0) {
+                forward = false;
                 b = String(button.textContent);
                 display.textContent = b;
-            } else if (b !== null && b.length >= 1) {
+            } else if (b !== null && b.length >= 1 && forward === false) {
                 b = b + String(button.textContent);
                 display.textContent = b;
             }
@@ -139,20 +159,15 @@ function update() {
 // If b does not equal 0 AND if operand does NOT equal '/'. This means that both need to be true to run the code
             if (a !== null && b !== null && operandButtonPressed) {
                 if (b === '0' && operand === '/'){
-                    console.log(b != 0);
-                    console.log(operand !== '/');
                     display.textContent = 'Zero Div Err';
-                    console.log(a);
-                    console.log(b);
-                    console.log(operand);
                     nullify();
                 } else {
                     sum = operate(+a, +b, operand);
                     sum = Math.round(sum * 100) / 100;
-
                     display.textContent = sum;
                     a = sum;
                     sumDisplayed = true;
+                    forward = true;
                     b = null;
                     operand = button.textContent;
                 }
